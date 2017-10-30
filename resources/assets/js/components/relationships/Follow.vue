105cdaf6de2d2310432db4b10bb6075931f79fcb
Takashi <leaves.saint@gmail.com>
2017-10-30 13:09:25 +0000

    follow button css modified

--- resources/assets/js/components/relationships/Follow.vue
+++ resources/assets/js/components/relationships/Follow.vue
@@ -7,12 +7,13 @@
             <td>
                 <!--<form action="{{secure_url('unfollow/' . $user->id)}}" method="POST">-->
                     <!--<button type="submit" id="delete-follow-{{ $user->target_id }}" class="btn btn-danger">-->
-                <button class="btn btn-primary a-btn" @click="reqUnfollow" :class="{ inactive: no}">
+                <button class="btn btn-primary a-btn" @click="reqUnfollow(f)" :class="{ inactive: no}">
                     <span></span>
                     <span>Following</span>
                     <span>Unfollow</span>
                 </button>
-                <button class="btn btn-success" @click="reqFollow"  :class="{ inactive: !no}">Follow</button>
+                <!--<button class="btn btn-success" @click="reqFollow(f.id)"  :class="{ inactive: !no}">Follow</button>-->
+                <button class="btn btn-success" @click="reqFollow(f)"  :class="{ inactive: !no}">Follow</button>
                 <!--</form>-->
             </td>
         </tr>
@@ -41,13 +42,17 @@
             ...mapActions({
                 loadUser: 'loadUser'
             }),
-            reqUnfollow() {
-                var id = this.f.id;
-                axios.delete('/unfollow/' + this.f.id);
+            reqUnfollow(user) {
+                axios.post('/unfollow/' + user.id);
                 this.no = !this.no;
             },
-            reqFollow() {
-                axios.post('/follow/' + this.f.id);
+            // reqFollow(id) {
+            //     // axios.post('/follow/' + this.f.id);
+            //     axios.post('/follow/' + id);
+            //     this.no = !this.no;
+            // },
+            reqFollow(user) {
+                axios.post('/follow/' + user.id);
                 this.no = !this.no;
             },
         }
@@ -64,53 +69,4 @@
         padding-left: 5px;
         padding-right: 5px;
     }
-    
-    /*------------------ 
-    | For Follow Button
-    -------------------*/
-        .inactive {
-            display: none;
-        }
-        
-        .a-btn:hover {
-            background-color: #ed5565;
-            border-color: #ed5565;
-        }
-        
-        .a-btn {
-            /*display: block;*/
-            float: left;
-            position: relative;
-            transition: all 0.3s linear;
-        }
-        
-        .a-btn span {
-            display: table-cell;
-            text-align: center;
-            vertical-align: middle;
-            transition: all 0.3s linear;
-        }
-        
-        .a-btn span:nth-child(1), 
-        .a-btn span:nth-child(3){
-            position: absolute;
-            top: 6px;
-            left: 12px;
-            line-height: 22.4px;
-            opacity: 0;
-        }
-        
-        .a-btn span:nth-child(1){
-            opacity: 0.2;
-        }
-        
-        .a-btn:hover span:nth-child(3){
-            opacity: 1; 
-        }
-        .a-btn:hover span:nth-child(2){
-            opacity: 0; 
-        }
-        .a-btn:hover span:nth-child(1){
-            animation: rotate 1s linear; 
-        }
 </style>
