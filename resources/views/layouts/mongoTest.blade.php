1f0fcbfca021420094b9888a2921a7ada518c4ce
Takashi <leaves.saint@gmail.com>
2018-04-15 20:26:39 +0000

    Add Bootstrap to testMongo.

--- resources/views/layouts/mongoTest.blade.php
+++ resources/views/layouts/mongoTest.blade.php
@@ -7,11 +7,13 @@
     
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+        
         <title>{{ config('app.name', 'Laravel') }}</title>
         
-        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <link href="{{ secure_asset('css2/app.css') }}" rel="stylesheet">
+        <!--<link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">-->
+
         <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
         <!-- Scripts -->
         <script>
@@ -21,66 +23,8 @@
         </script>
     </head>
     <body>
-        <div id="wrapper" class="nonedit">
-            <div id="app">
-                <nav class="navbar navbar-default navbar-static-top">
-                    <div class="container">
-                        <div class="navbar-header">
-        
-                            <!-- Collapsed Hamburger -->
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                                <span class="sr-only">Toggle Navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-        
-                            <!-- Branding Image -->
-                            <!--<a class="navbar-brand" href="{{ url('/') }}">-->
-                            <!--    {{ config('app.name', 'Laravel') }}-->
-                            <!--</a>-->
-                        </div>
-        
-                        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                            <!-- Left Side Of Navbar -->
-                            <ul class="nav navbar-nav">
-                                &nbsp;
-                            </ul>
-        
-                            <!-- Right Side Of Navbar -->
-                            <ul class="nav navbar-nav navbar-right">
-                                <!-- Authentication Links -->
-                                @if (Auth::guest())
-                                    <li><a href="{{ route('login') }}">Login</a></li>
-                                    <li><a href="{{ route('register') }}">Register</a></li>
-                                @else
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                            {{ Auth::user()->name }} <span class="caret"></span>
-                                        </a>
-        
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li>
-                                                <a href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                                    Logout
-                                                </a>
-        
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                    {{ csrf_field() }}
-                                                </form>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                @endif
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                @yield('content')
-                    
-            </div>
+        <div id="app">
+            @yield('content')
         </div>
     </body>
     <script
