66763e038ac5d447c30795f2240ce1917517e4c9
Takashi <leaves.saint@gmail.com>
2018-04-25 11:42:57 +0000

    Improve reactive display size getter and setter.

--- resources/assets/js/mixins/displayMixin.js
+++ resources/assets/js/mixins/displayMixin.js
@@ -1,24 +1,35 @@
 /* To change content by dispaly size */
 
 export const displayMixin = {
-    computed: {
-        isLarge() {
+    data() {
+        return {
+            isLarge: '',
+            isNotLarge: ''
+        }
+    },
+    mounted() {
+        if(window.matchMedia('(min-width: 1024px)').matches)
+        {
+            this.isLarge = true;
+            this.isNotLarge = false;
+        } else
+        {
+            this.isLarge = false;
+            this.isNotLarge = true;
+        }
+        window.addEventListener('resize', this.screen);
+    },
+    methods: {
+        screen: function () {
             if(window.matchMedia('(min-width: 1024px)').matches)
             {
-                return true;
+                this.isLarge = true;
+                this.isNotLarge = false;
             } else
             {
-                return false;
-            }
-        },
-        isNotLarge() {
-            if(window.matchMedia('(max-width: 1024px)').matches)
-            {
-                return true;
-            } else
-            {
-                return false;
+                this.isLarge = false;
+                this.isNotLarge = true;
             }
         }
-    }
+    },
 }
